f298e22743c22a5ec151f4af45ce0e08cc9b4bdf
EricSebastianesENTI <eric.sebastianes@estudiant.enti.cat>
2026-01-06 00:30:52 +0100

    lobby created and tested

--- ubuntus/src/routes/_routes.js
+++ ubuntus/src/routes/_routes.js
@@ -1,15 +1,21 @@
 const { Router } = require("express");
 const router = Router();
+const path = require("path");
 
 router.get("/", (req, res) => {
-    res.send("Columns Online Server - Running");
+    // Redirigir al lobby
+    res.redirect("/lobby");
+});
+
+router.get("/lobby", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/lobby.html"));
 });
 
 router.use("/characters", require("./characters"));
 router.use("/weapons", require("./weapons"));
 router.use("/chat", require("./chat"));
 
-
+// API REST
 router.use("/api", require("./api"));
 
 module.exports = router;
