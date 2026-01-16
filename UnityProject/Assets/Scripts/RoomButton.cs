c8532cac71b044a7208a0831d7fc0bf1cd3300c9
EricSebastianesENTI <eric.sebastianes@estudiant.enti.cat>
2026-01-16 15:31:15 +0100

    some fixes on button's prefab

--- UnityProject/Assets/Scripts/RoomButton.cs
+++ UnityProject/Assets/Scripts/RoomButton.cs
@@ -8,8 +8,7 @@ public class RoomButton : MonoBehaviour, IPointerClickHandler
     public string roomName = "";
 
     public void OnPointerClick(PointerEventData eventData)
-    {
-        
+    {        
        GameClient.Instance.JoinRoom(roomName);
        GameClient.Instance.currentRoomName = roomName;
     }
